6168e2faeab0fd7a0a15d8ebd07ec1cb174e91c4
Link4Electronics <link4electronics@gmail.com>
2026-02-02 16:04:18 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,7 +15,11 @@ export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/Cemu
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+  quick-sharun /usr/bin/Cemu
+else
+  quick-sharun /usr/bin/cemu
+fi
 
 # Additional changes can be done in between here
 
