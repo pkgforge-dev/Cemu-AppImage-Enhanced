8ec5bd09dc10d5b240b9bc4d8024de0725e0678d
Link4Electronics <link4electronics@gmail.com>
2026-02-02 04:55:19 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -56,7 +56,7 @@ make-aur-package cubeb
 
 git clone --recursive https://github.com/cemu-project/Cemu
 
-cd cemu
+cd Cemu
 cmake -D ALLOW_PORTABLE=OFF \
 		-D CMAKE_BUILD_TYPE=Release \
 		-D CMAKE_C_FLAGS_RELEASE="-DNDEBUG" \
