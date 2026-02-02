c36633f632bdc6bc4f34299d899c22dd5b88b6b0
Link4Electronics <link4electronics@gmail.com>
2026-02-02 16:08:32 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,13 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook:x86-64-v3-check.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/info.cemu.Cemu.png
-export DESKTOP=/usr/share/applications/info.cemu.Cemu.desktop
+
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+  export DESKTOP=/usr/share/applications/info.cemu.Cemu.desktop
+else
+  export DESKTOP=/usr/share/applications/Cemu.desktop
+fi
+
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
