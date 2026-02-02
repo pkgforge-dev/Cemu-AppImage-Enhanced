058f3d4eb5fddbcceb75f6759fbfa97c7e3e4d40
Link4Electronics <link4electronics@gmail.com>
2026-02-02 15:39:23 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,34 +11,24 @@ pacman -Syu --noconfirm \
 	boost               \
 	clang               \
 	cmake               \
-	curl                \
 	fmt                 \
-	gcc-libs			\
 	glm                 \
 	glslang             \
 	glu                 \
-	gtk3                \
 	hicolor-icon-theme  \
 	hidapi              \
 	libgl               \
-	libpng              \
-	libusb				\
-	libx11              \
 	libzip              \
 	llvm                \
 	nasm                \
-	openssl             \
 	pipewire-audio      \
 	pipewire-jack       \
 	pugixml             \
 	rapidjson           \
 	sdl2				\
 	vulkan-headers      \
-	wayland             \
 	wayland-protocols   \
-	zarchive            \
-	zlib                \
-	zstd
+	zarchive
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -51,42 +41,51 @@ echo "Building Cemu..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/cemu-project/Cemu"
 # Determine to build nightly or stable
+#if [ "${DEVEL_RELEASE-}" = 1 ]; then
+#	echo "Making nightly build of Cemu..."
+#	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+#    git clone --recursive --depth 1 "$REPO" ./Cemu
+#else
+#	echo "Making stable build of Cemu..."
+#	VERSION="$(git ls-remote --tags --sort="v:refname" https://github.com/cemu-project/Cemu | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')"
+#	git clone --branch v"$VERSION" --single-branch --recursive --depth 1 "$REPO" ./Cemu
+#fi
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
 	echo "Making nightly build of Cemu..."
 	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
     git clone --recursive --depth 1 "$REPO" ./Cemu
+
+	cd Cemu
+	mkdir -p build && cd build
+
+
+	if [ "$ARCH" = 'x86_64' ]; then
+		echo "Making x86-64-v3 optimized build of Cemu..."
+		ARCH_FLAGS="-march=x86-64-v3 -O3"
+	fi
+
+	cmake .. -D ALLOW_PORTABLE=OFF \
+		  -D CMAKE_BUILD_TYPE=Release \
+	  	  -D CMAKE_C_COMPILER=clang \
+	 	  -D CMAKE_CXX_COMPILER=clang++ \
+		  -D CMAKE_EXE_LINKER_FLAGS="-lzstd" \
+	 	  -D CMAKE_C_FLAGS_RELEASE="$ARCH_FLAGS -DNDEBUG" \
+		  -D CMAKE_CXX_FLAGS_RELEASE="$ARCH_FLAGS -DNDEBUG" \
+		  -D CMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
+	 	  -D CMAKE_POLICY_VERSION_MINIMUM=3.5 \ # Required for stable
+		  -D ENABLE_VCPKG=OFF \
+		  -Wno-dev -B build
+	make -j $(nproc)
+
+	install -d /usr/bin /usr/share/Cemu
+	mv bin/Cemu_release /usr/bin/Cemu
+	cp -dr --no-preserve=ownership -t /usr/share/Cemu bin/*
+	install -Dm644 -t /usr/share/applications dist/linux/info.cemu.Cemu.desktop
+	install -Dm644 -t /usr/share/icons/hicolor/128x128/apps dist/linux/info.cemu.Cemu.png
+	install -Dm644 -t /usr/share/metainfo dist/linux/info.cemu.Cemu.metainfo.xml
 else
 	echo "Making stable build of Cemu..."
-	VERSION="$(git ls-remote --tags --sort="v:refname" https://github.com/cemu-project/Cemu | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')"
-	git clone --branch v"$VERSION" --single-branch --recursive --depth 1 "$REPO" ./Cemu
+	TARGET_V3_CPU=1 make-aur-package cemu
+	VERSION=$(pacman -Q cemu | awk '{print $2; exit}')
 fi
 echo "$VERSION" > ~/version
-
-cd Cemu
-mkdir -p build && cd build
-
-
-if [ "$ARCH" = 'x86_64' ]; then
-	echo "Making x86-64-v3 optimized build of Cemu..."
-	ARCH_FLAGS="-march=x86-64-v3 -O3"
-fi
-
-cmake .. -D ALLOW_PORTABLE=OFF \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_C_COMPILER=clang \
-	  -D CMAKE_CXX_COMPILER=clang++ \
-	  -D CMAKE_EXE_LINKER_FLAGS="-lzstd" \
-	  -D CMAKE_C_FLAGS_RELEASE="$ARCH_FLAGS -DNDEBUG" \
-	  -D CMAKE_CXX_FLAGS_RELEASE="$ARCH_FLAGS -DNDEBUG" \
-	  -D CMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
-	  -D CMAKE_POLICY_VERSION_MINIMUM=3.5 \ # Required for stable
-	  -D ENABLE_VCPKG=OFF \
-	  -Wno-dev -B build
-make -j $(nproc)
-
-install -d /usr/bin /usr/share/Cemu
-mv bin/Cemu_release /usr/bin/Cemu
-cp -dr --no-preserve=ownership -t /usr/share/Cemu bin/*
-install -Dm644 -t /usr/share/applications dist/linux/info.cemu.Cemu.desktop
-install -Dm644 -t /usr/share/icons/hicolor/128x128/apps dist/linux/info.cemu.Cemu.png
-install -Dm644 -t /usr/share/metainfo dist/linux/info.cemu.Cemu.metainfo.xml
