50eccca7a6a9f6cd0d7a564a5a552f479e5a2c10
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-03 03:01:11 -0400

    refactor

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,7 +15,6 @@ pacman -Syu --noconfirm \
 	glm                 \
 	glslang             \
 	glu                 \
-	hicolor-icon-theme  \
 	hidapi              \
 	libgl               \
 	libzip              \
@@ -40,53 +39,42 @@ make-aur-package wxgtk-git
 echo "Building Cemu..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/cemu-project/Cemu"
-# Determine to build nightly or stable
-#if [ "${DEVEL_RELEASE-}" = 1 ]; then
-#	echo "Making nightly build of Cemu..."
-#	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-#    git clone --recursive --depth 1 "$REPO" ./Cemu
-#else
-#	echo "Making stable build of Cemu..."
-#	VERSION="$(git ls-remote --tags --sort="v:refname" https://github.com/cemu-project/Cemu | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')"
-#	git clone --branch v"$VERSION" --single-branch --recursive --depth 1 "$REPO" ./Cemu
-#fi
+
+if [ "$ARCH" = 'x86_64' ]; then
+	echo "Making x86-64-v3 optimized build of Cemu..."
+	set -- -march=x86-64-v3 -O3 -DNDEBUG
+else
+	set -- -DNDEBUG
+fi
+
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
 	echo "Making nightly build of Cemu..."
-	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
     git clone --recursive --depth 1 "$REPO" ./Cemu
+	mkdir -p ./Cemu/build
+	cd ./Cemu/build
+	git rev-parse --short=9 HEAD > ~/version
 
-	cd Cemu
-	mkdir -p build && cd build
-
-	ARCH_FLAGS=""
-	if [ "$ARCH" = 'x86_64' ]; then
-		echo "Making x86-64-v3 optimized build of Cemu..."
-		ARCH_FLAGS="-march=x86-64-v3 -O3"
-	fi
-	
 	# CMAKE_POLICY_VERSION_MINIMUM=3.5 required for stable
-	cmake .. -D ALLOW_PORTABLE=OFF \
-		  -D CMAKE_BUILD_TYPE=Release \
-	  	  -D CMAKE_C_COMPILER=clang \
-	 	  -D CMAKE_CXX_COMPILER=clang++ \
-		  -D CMAKE_EXE_LINKER_FLAGS="-lzstd" \
-	 	  -D CMAKE_C_FLAGS_RELEASE="$ARCH_FLAGS -DNDEBUG" \
-		  -D CMAKE_CXX_FLAGS_RELEASE="$ARCH_FLAGS -DNDEBUG" \
-		  -D CMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
-	 	  -D CMAKE_POLICY_VERSION_MINIMUM=3.5 \ 
-		  -D ENABLE_VCPKG=OFF \
-		  -Wno-dev
+	cmake ../ \
+		-D ALLOW_PORTABLE=OFF                    \
+		-D CMAKE_BUILD_TYPE=Release              \
+		-D CMAKE_C_COMPILER=clang                \
+		-D CMAKE_CXX_COMPILER=clang++            \
+		-D CMAKE_EXE_LINKER_FLAGS="-lzstd"       \
+		-D CMAKE_C_FLAGS_RELEASE="$@"            \
+		-D CMAKE_CXX_FLAGS_RELEASE="$@"          \
+		-D CMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
+		-D CMAKE_POLICY_VERSION_MINIMUM=3.5      \ 
+		-D ENABLE_VCPKG=OFF                      \
+		-Wno-dev
 	make -j $(nproc)
+	
+	cp -v ./bin/Cemu_release /usr/bin/cemu
 
-	install -d /usr/bin /usr/share/Cemu
-	mv bin/Cemu_release /usr/bin/Cemu
-	cp -dr --no-preserve=ownership -t /usr/share/Cemu bin/*
-	install -Dm644 -t /usr/share/applications dist/linux/info.cemu.Cemu.desktop
-	install -Dm644 -t /usr/share/icons/hicolor/128x128/apps dist/linux/info.cemu.Cemu.png
-	install -Dm644 -t /usr/share/metainfo dist/linux/info.cemu.Cemu.metainfo.xml
+	sed -i -e 's|Exec=Cemu|Exec=cemu|g' ./dist/linux/info.cemu.Cemu.desktop
+	cp -v ./dist/linux/info.cemu.Cemu.desktop /usr/share/applications/Cemu.desktop
 else
 	echo "Making stable build of Cemu..."
 	TARGET_V3_CPU=1 make-aur-package cemu
-	VERSION=$(pacman -Q cemu | awk '{print $2; exit}')
+	pacman -Q cemu | awk '{print $2; exit}'  > ~/version
 fi
-echo "$VERSION" > ~/version
