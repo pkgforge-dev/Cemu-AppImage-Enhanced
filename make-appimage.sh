2923307b5efc5ffd9c4c2c7697b205be543c3683
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-03 03:04:14 -0400

    refactor

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,29 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-#VERSION=$(pacman -Q cemu | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook:x86-64-v3-check.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/128x128/apps/info.cemu.Cemu.png
-
-if [ "${DEVEL_RELEASE-}" = 1 ]; then
-  export DESKTOP=/usr/share/applications/info.cemu.Cemu.desktop
-else
-  export DESKTOP=/usr/share/applications/Cemu.desktop
-fi
-
+export ICON=https://raw.githubusercontent.com/cemu-project/Cemu/refs/heads/main/dist/linux/info.cemu.Cemu.png
+export DESKTOP=/usr/share/applications/Cemu.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-if [ "${DEVEL_RELEASE-}" = 1 ]; then
-  quick-sharun /usr/bin/Cemu
-else
-  quick-sharun /usr/bin/cemu
-fi
+quick-sharun /usr/bin/cemu
 
 # Additional changes can be done in between here
 
